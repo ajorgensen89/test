78eb19e632a288a43bf9bf8f2db0f4dea3eebfd2
ajorgensen89 <ajorgensen89@gmail.com>
2023-07-26 15:15:23 +0000

    ES7 snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,59 @@
+// rfce hit enter = import, export, start of a function
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// rafce = arrow func
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// rcs = class component. Imports Componant directly.
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+// rcon = construtor
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+// clg = console.log
+
+// console.log(first)
+
+// clo = name and extra
+
+// console.log('first', first)
